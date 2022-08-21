f11d5c2c5ecec6e207cff919b1c0c5d0c56dbd60
Charlytoc <charlyjchaconc@gmail.com>
2022-08-21 14:22:48 +0000

    card generator with button

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let icon1 = document.getElementById("upCard");
+  let icon2 = document.getElementById("footCard");
+  let randomIndex = Math.floor(Math.random() * 4);
+  let iconsArr = ["♦", "♥", "♠", "♣"];
+  icon1.innerHTML = `<p>${iconsArr[randomIndex]}`;
+  icon2.innerHTML = `<p>${iconsArr[randomIndex]}`;
+
+  let numberOfCard = document.getElementById("numberCard");
+  let indexCard = Math.floor(Math.random() * 10);
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "J", "Q", "K"];
+  numberOfCard.innerHTML = `<p>${cardNumber[indexCard]}</p>`;
 };
+
+function changeCard() {
+  let icon1 = document.getElementById("upCard");
+  let icon2 = document.getElementById("footCard");
+  let randomIndex = Math.floor(Math.random() * 4);
+  let iconsArr = ["♦", "♥", "♠", "♣"];
+  icon1.innerHTML = `<p>${iconsArr[randomIndex]}`;
+  icon2.innerHTML = `<p>${iconsArr[randomIndex]}`;
+
+  let numberOfCard = document.getElementById("numberCard");
+  let indexCard = Math.floor(Math.random() * 10);
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "J", "Q", "K"];
+  numberOfCard.innerHTML = `<p>${cardNumber[indexCard]}</p>`;
+}
+let buttonToChange = document.getElementById("buttonChange");
+buttonToChange.addEventListener("click", changeCard);
